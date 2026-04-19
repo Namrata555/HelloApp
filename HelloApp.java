373ada35043bf96fb8f332cac19741fa0f0d2c81
Namrata555 <namratakuila555@gmail.com>
2026-04-19 08:33:52 +0530

    UC7 implemented using String.join method

--- HelloApp.java
+++ HelloApp.java
@@ -2,23 +2,11 @@ public class HelloApp {
 
     public static void main(String[] args) {
 
-        // Check if no arguments are provided
         if (args.length == 0) {
             System.out.println("Hello, World!");
-        } else {
-
-            // StringBuilder to build names
-            StringBuilder nameBuilder = new StringBuilder();
-
-            // Enhanced for loop to add names
-            for (String name : args) {
-                nameBuilder.append(name).append(", ");
-            }
-
-            // Remove trailing comma and space using substring
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-
-            // Print final greeting
+        } 
+        else {
+            String names = String.join(", ", args);
             System.out.println("Hello, " + names + "!");
         }
     }
